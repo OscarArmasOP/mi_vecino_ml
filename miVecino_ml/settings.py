fff91755ab688217c13d70e17c53f3453b1b15c4
Oscar <oma_1506@hotmail.com>
2022-11-09 02:34:10 -0600

    heroku

--- miVecino_ml/settings.py
+++ miVecino_ml/settings.py
@@ -17,12 +17,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'default_value')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-# ALLOWED_HOSTS = [".herokuapp.com"]
+ALLOWED_HOSTS = [".herokuapp.com"]
 
 
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = []
 
 # Application definition
 
